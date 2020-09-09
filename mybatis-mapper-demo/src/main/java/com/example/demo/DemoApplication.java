be91be98309d812c3f4f4251b5f90491a8df51dd
xccu <264672790@qq.com>
2020-09-09 20:26:15 +0800

    添加json缩进功能

--- mybatis-mapper-demo/src/main/java/com/example/demo/DemoApplication.java
+++ mybatis-mapper-demo/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -10,5 +12,14 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    /**
+     * 返回缩进后的json字符串，提升可读性
+     * @return
+     */
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jacksonBuilderCustomizer() {
+        return builder -> builder.indentOutput(true);
     }
 }
