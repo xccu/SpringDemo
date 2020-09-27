f7885d2b53548c1bfd42e5fa07ff945525298f02
xiecangyi <xiecy@travelsky.com>
2020-09-27 16:09:28 +0800

    添加注释

--- springmvc-echarts-demo/src/main/java/com/example/demo/config/WebMvcConfig.java
+++ springmvc-echarts-demo/src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -14,19 +14,30 @@ public class WebMvcConfig  extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
 
+        //柱状图
+        //http://localhost:8090/bar/demo1
+        //registry.addViewController("/bar/demo1").setViewName("/bar/bar-demo-1");
+
         //折线图
+        //http://localhost:8090/line/demo1
         registry.addViewController("/line/demo1").setViewName("/line/line-demo-1");
+        //http://localhost:8090/line/demo2
         registry.addViewController("/line/demo2").setViewName("/line/line-demo-2");
 
         //饼图
+        //http://localhost:8090/pie/demo1
         registry.addViewController("/pie/demo1").setViewName("/pie/pie-demo-1");
 
         //仪表盘
+        //http://localhost:8090/gauge/demo1
         registry.addViewController("/gauge/demo1").setViewName("/gauge/gauge-demo-1");
+        //http://localhost:8090/gauge/demo2
         registry.addViewController("/gauge/demo2").setViewName("/gauge/gauge-demo-2");
 
         //日历
+        //http://localhost:8090/calendar/demo1
         registry.addViewController("/calendar/demo1").setViewName("/calendar/calendar-demo-1");
+        //http://localhost:8090/calendar/demo2
         registry.addViewController("/calendar/demo2").setViewName("/calendar/calendar-demo-2");
     }
 
