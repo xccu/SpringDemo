28f5ac82b0bbd32199aec4c77981690a2a6c2276
xiecangyi <xiecy@travelsky.com>
2020-11-04 17:23:05 +0800

    modify

--- spring-taskscheduler-demo/src/main/java/com/example/Main.java
+++ spring-taskscheduler-demo/src/main/java/com/example/Main.java
@@ -7,16 +7,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
 	public static void main(String[] args)  {
 
+		log.info("开始");
 		//使用AnnotationConfigApplicationContext实现基于Java的配置类加载Spring的应用上下文
 		AnnotationConfigApplicationContext context =
 				new AnnotationConfigApplicationContext(TaskSchedulerConfig.class);
 
-
-		/*while(true) {
-			Thread.sleep(3000);
-			log.info("Hello");
-		}*/
-		 
 	}
-
 }
