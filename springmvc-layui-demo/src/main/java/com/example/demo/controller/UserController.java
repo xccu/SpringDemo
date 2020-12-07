9789025c1a116b9a45de7e2098e79ff04aec57b0
xiecangyi <xiecy@travelsky.com>
2020-12-07 17:19:43 +0800

    modified springmvc-layui-demo

--- springmvc-layui-demo/src/main/java/com/example/demo/controller/UserController.java
+++ springmvc-layui-demo/src/main/java/com/example/demo/controller/UserController.java
@@ -6,6 +6,7 @@ import net.sf.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -26,6 +27,7 @@ public class UserController {
 
     /**
      * josn格式字符串
+     * url:"http://localhost:8080/user/allStr"
      * @return
      */
     @ResponseBody
@@ -46,12 +48,7 @@ public class UserController {
                 "                {\"id\":7,\"name\":\"Wolffy\",\"age\":34,\"sex\":\"male\"},\n" +
                 "                {\"id\":8,\"name\":\"Tibbie\",\"age\":11,\"sex\":\"female\"},\n" +
                 "                {\"id\":9,\"name\":\"Sparky\",\"age\":12,\"sex\":\"male\"},\n" +
-                "                {\"id\":10,\"name\":\"Paddi\",\"age\":10,\"sex\":\"male\"},\n" +
-                "                {\"id\":11,\"name\":\"Weslie\",\"age\":12,\"sex\":\"male\"},\n" +
-                "                {\"id\":12,\"name\":\"Wolffy\",\"age\":34,\"sex\":\"male\"},\n" +
-                "                {\"id\":13,\"name\":\"Tibbie\",\"age\":11,\"sex\":\"female\"},\n" +
-                "                {\"id\":14,\"name\":\"Sparky\",\"age\":12,\"sex\":\"male\"},\n" +
-                "                {\"id\":15,\"name\":\"Paddi\",\"age\":10,\"sex\":\"male\"}\n" +
+                "                {\"id\":10,\"name\":\"Paddi\",\"age\":10,\"sex\":\"male\"}\n" +
                 "     ]\n" +
                 "}\n";
         return result;
@@ -59,6 +56,7 @@ public class UserController {
 
     /**
      * json对象
+     * url:"http://localhost:8080/user/allJson"
      * @return
      */
     @ResponseBody
@@ -66,6 +64,29 @@ public class UserController {
     public JSONObject getAllUserJson(){
 
         List<User> users= getUserList();
+        //在传入中，为了满足Layui的格式要求，加上了一些头部：code、msg、count、data
+        JSONObject json = new JSONObject();
+        json.put("msg", "success");
+        json.put("code", "0");
+        json.put("status", "200");
+        json.put("count", users.size());
+        json.put("data", users);
+        return json;
+    }
+
+    /**
+     * 带参数的get请求
+     * url:"http://localhost:8080/user/getByName?name={name}"
+     * @param name
+     * @return
+     */
+    @ResponseBody
+    @GetMapping(value = "/getByName")
+    public JSONObject getByName(String name){
+
+        List<User> users= getUserList();
+        users = users.stream().filter(t->t.getName().equals(name)).collect(Collectors.toList());
+
         //在传入中，为了满足Layui的格式要求，加上了一些头部：code、msg、count、data
         JSONObject json = new JSONObject();
         json.put("msg", "success");
@@ -132,49 +153,49 @@ public class UserController {
 
 
         user = new User();
-        user.setId(1);
+        user.setId(8);
         user.setName("Weslie");
         user.setAge(12);
         user.setSex("male");
         userList.add(user);
 
         user = new User();
-        user.setId(2);
+        user.setId(9);
         user.setName("Wolffy");
         user.setAge(34);
         user.setSex("male");
         userList.add(user);
 
         user = new User();
-        user.setId(3);
+        user.setId(10);
         user.setName("Tibbie");
         user.setAge(11);
         user.setSex("female");
         userList.add(user);
 
         user = new User();
-        user.setId(4);
+        user.setId(11);
         user.setName("Sparky");
         user.setAge(12);
         user.setSex("male");
         userList.add(user);
 
         user = new User();
-        user.setId(5);
+        user.setId(12);
         user.setName("Paddi");
         user.setAge(10);
         user.setSex("male");
         userList.add(user);
 
         user = new User();
-        user.setId(6);
+        user.setId(13);
         user.setName("Wilie");
         user.setAge(5);
         user.setSex("male");
         userList.add(user);
 
         user = new User();
-        user.setId(7);
+        user.setId(14);
         user.setName("Wolnie");
         user.setAge(33);
         user.setSex("female");
